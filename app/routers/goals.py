809c0d969913fc0c28d1a667cc0d70925f46d902
Filip Paryż <paryz.filip@gmail.com>
2023-12-10 12:36:10 +0100

    Removed status code

--- app/routers/goals.py
+++ app/routers/goals.py
@@ -114,18 +114,18 @@ async def post_progress_goal(goal_id: str, progress: float):
     return
 
 @router.post("/goals/{goal_id}/archive", tags=["goals"])
-async def archive_goal(goal_id: str) -> (int, Goal):
+async def archive_goal(goal_id: str) -> Goal:
 
     goal = get_goal(goal_id)
 
     if goal is None:
-        return (404, None)
+        return None
     
     goal.archived = not goal.archived
     
     update_goal(goal_id, goal)
     
-    return (200, goal)
+    return goal
 
 @router.post("/goals/{goal_id}/complete", tags=["goals"])
 async def complete_goal(goal_id: str):
